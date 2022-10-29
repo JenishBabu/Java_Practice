68fadbd248464d0a84f0e04214ae0b8b430d1224
JenishBabu <110540665+JenishBabu@users.noreply.github.com>
2022-10-29 14:12:00 +0530

    Create 02-Conditionals_Loops.java

---
+++ 02-Conditionals_Loops.java
@@ -0,0 +1,92 @@
+//Write Java programs for the following:
+
+// 1. Area Of Circle Java Program
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double pi=22.0/7;
+        System.out.println(pi*r*r);
+    }
+}
+
+// 2.Area Of Triangle.
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int b,h;
+        b=sc.nextInt();
+        h=sc.nextInt();
+        System.out.println((b*h)/2);
+    }
+}
+
+// 3.Area Of Rectangle Program.
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int b,l;
+        b=sc.nextInt();
+        l=sc.nextInt();
+        System.out.println(l*b);
+    }
+}
+
+// 4.Area Of Isosceles Triangle
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int b,h;
+        b=sc.nextInt();
+        h=sc.nextInt();
+        System.out.println((b*h)/2);
+    }
+}
+
+// 5.Area Of Parallelogram
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int b,h;
+        b=sc.nextInt();
+        h=sc.nextInt();
+        System.out.println(h*b);
+    }
+}
+
+//  6.Area Of Rhombus
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int d1,d2;
+        d1=sc.nextInt();
+        d2=sc.nextInt();
+        System.out.println((d1*d2)/2);
+    }
+}
+
+// 7.Area Of Equilateral Triangle 
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = Math.sqrt(3)/4;
+        System.out.println(b*a*a);
+    }
+}
