2321fc15979e97a3d8504e9bc378b269550e25f8
JenishBabu <110540665+JenishBabu@users.noreply.github.com>
2022-11-12 19:27:56 +0530

    Update Loops.java

--- 02-conditionals-loops/Loops.java
+++ 02-conditionals-loops/Loops.java
@@ -19,3 +19,56 @@ public class Main {
     }
 }
 
+// 22.Subtract the Product and Sum of Digits of an Integer
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int s=0;
+        int p=1;
+        while(a>0){
+            int n = a%10;
+            s=s+n;
+            p=p*n;
+            a=a/10;
+
+        }
+        System.out.println(p-s);
+
+    }
+}
+
+// 23.Input a number and print all the factors of that number 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
+
+// 24.Take integer inputs till the user enters 0 and print the sum of all numbers 
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s=0;
+        int n;
+        while((n=sc.nextInt())>0){
+            if(n!=0){
+                s=s+n;
+            }
+        }
+        System.out.print(s);
+    }
+}
+
+// 25.
